158f7973256e913f9d28ba406f795114424af999
matheussbs <76970330+matheussbs@users.noreply.github.com>
2021-05-25 17:52:02 -0300

    third commit

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const port = process.env.PORT || 3000
 const indexRouter = require('./routes/index')
 const tabuadaRouter = require('./routes/tabuada')
 
-app.use(express.static('public'))
+//app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, 'public'))) //vercel
 
 //controllers
 
